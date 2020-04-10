54550e26ae29a317145e0907c9eae5b5ca668483
lukacukeric77 <lukacukeric77@gmail.com>
2020-04-10 14:02:25 +0200

    Boilerplate, before starting

---
+++ src/main/java/be/vdab/beerhousewithjpa/BeerhousewithjpaApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.beerhousewithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerhousewithjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerhousewithjpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/beerhousewithjpa/BeerhousewithjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.beerhousewithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerhousewithjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
